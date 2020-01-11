f5ded9b5b7d9cf6721cec06e9c292fc803fafbae
yuka <yuka@yuka.local>
2020-01-11 19:18:41 +0900

    [Finish] 5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '535a9e6319c3bef35ded72c8cc1e27d749244fdb34a6fcd4803b66dac84d3d54e7e28f300a2f3db7e37070dc6ba954a3bb7e0a4f8c8845113aa9ee759e853eac'
